7e8b674623b2755cae44faee30eda451ebd4c1ab
Wojciech Nowak <wojtekn00@gmail.com>
2023-04-12 16:50:19 +0200

    pagination added to the table

--- src/Table/StatsTable.tsx
+++ src/Table/StatsTable.tsx
@@ -10,6 +10,7 @@ import {
   TablePagination,
 } from '@material-ui/core';
 import { StylesProvider } from '@material-ui/core/styles';
+import { useState } from 'react';
 
 interface CountrySummary {
   Country: string;
@@ -40,6 +41,28 @@ interface StatsTableProps {
 }
 
 const StatsTable = (props: StatsTableProps) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const combinedFetchedDataInArray = [
+    props.globalSummary,
+    ...props.summaryArray,
+  ];
+
   return (
     <>
       <StylesProvider injectFirst>
@@ -47,7 +70,7 @@ const StatsTable = (props: StatsTableProps) => {
           <TableContainer component={Paper}>
             <Table aria-label="simple table">
               <TableHead>
-                <TableRow>
+                <TableRow className="header-row">
                   <TableCell>#</TableCell>
                   <TableCell>Country/Global</TableCell>
                   <TableCell>Total Cases</TableCell>
@@ -81,34 +104,44 @@ const StatsTable = (props: StatsTableProps) => {
                     {props.globalSummary.NewRecovered.toLocaleString()}
                   </TableCell>
                 </TableRow>
-                {props.summaryArray.map((countrySummary, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{index + 2}</TableCell>
-                    <TableCell>{countrySummary.Country}</TableCell>
-                    <TableCell>
-                      {countrySummary.TotalConfirmed.toLocaleString()}
-                    </TableCell>
-                    <TableCell>
-                      {countrySummary.NewConfirmed.toLocaleString()}
-                    </TableCell>
-                    <TableCell>
-                      {countrySummary.TotalDeaths.toLocaleString()}
-                    </TableCell>
-                    <TableCell>
-                      {countrySummary.NewDeaths.toLocaleString()}
-                    </TableCell>
-                    <TableCell>
-                      {countrySummary.TotalRecovered.toLocaleString()}
-                    </TableCell>
-                    <TableCell>
-                      {countrySummary.NewRecovered.toLocaleString()}
-                    </TableCell>
-                  </TableRow>
-                ))}
+                {props.summaryArray
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((countrySummary, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{index + 1}</TableCell>
+                      <TableCell>{countrySummary.Country}</TableCell>
+                      <TableCell>
+                        {countrySummary.TotalConfirmed.toLocaleString()}
+                      </TableCell>
+                      <TableCell>
+                        {countrySummary.NewConfirmed.toLocaleString()}
+                      </TableCell>
+                      <TableCell>
+                        {countrySummary.TotalDeaths.toLocaleString()}
+                      </TableCell>
+                      <TableCell>
+                        {countrySummary.NewDeaths.toLocaleString()}
+                      </TableCell>
+                      <TableCell>
+                        {countrySummary.TotalRecovered.toLocaleString()}
+                      </TableCell>
+                      <TableCell>
+                        {countrySummary.NewRecovered.toLocaleString()}
+                      </TableCell>
+                    </TableRow>
+                  ))}
               </TableBody>
             </Table>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component={Paper}
+              count={combinedFetchedDataInArray.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
           </TableContainer>
-          {/* <TablePagination /> */}
         </div>
       </StylesProvider>
     </>
